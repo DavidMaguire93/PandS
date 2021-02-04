f1b1cc17f877cdb3c2cff25d7831c1a2ae53cdce
David Maguire <G00398328gmit.ie>
2021-02-04 19:57:24 +0000

    week03Exercise

---
+++ week03/secondString.py
@@ -0,0 +1,11 @@
+# Program that takes asks a user to input a string and outputs every second letter in reverse order
+#Author: David Maguire
+
+# Ask for sentance
+rawString = input("Please enter a sentance: ")
+
+# Reverse string by starting from end and going backwards every 2nd char
+reverseSkip = rawString[::-2]
+
+# Print result
+print(reverseSkip)
