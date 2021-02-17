2c742e65c7a8c0997f200b6fa3f1e60cb3932945
David Maguire <G00398328gmit.ie>
2021-02-17 18:19:32 +0000

    week05WeeklyTask

---
+++ week05/weekday.py
@@ -0,0 +1,21 @@
+# Program that outputs whether or not today is a weekday
+# Author: David Maguire
+
+# Import datetime for today's date function
+import datetime
+
+# Use strftime to get a string of today's date
+# %A is a string format for the name of the day of the given date
+today = datetime.datetime.today().strftime('%A')
+
+# Create list of weekdays and weekend (weekend is not actually needed)
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"] 
+weekend = ["Saturday", "Sunday"]
+
+# If today is in list of weekdays print weekday response
+if today in weekdays:
+    print("Yes, unfortunately today is a weekday.")
+
+# Otherwise, print other response
+else:
+    print("It is the weekend, yay!")
