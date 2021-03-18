159dfdc4e2c6ab2dd34db299d7da1548025bf255
David Maguire <G00398328gmit.ie>
2021-03-18 18:40:11 +0000

    First look at Iris dataset

---
+++ pands-project2021/exploringIris.py
@@ -0,0 +1,8 @@
+# Exploring Iris
+# Code taken from https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html
+
+# Load data and print
+from sklearn.datasets import load_iris
+data = load_iris()
+
+print(data)
